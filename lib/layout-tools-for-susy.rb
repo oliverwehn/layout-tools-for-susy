42e153d426bbe45f77eaf59e0d49cff305f19235
Oliver Wehn <hello@oliverwehn.com>
2015-11-29 18:41:28 +0100

    Added tool files.

---
+++ lib/layout-tools-for-susy.rb
@@ -0,0 +1,17 @@
+base_directory  = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+layout_tools_stylesheets_path = File.join(base_directory, 'stylesheets')
+
+if (defined? Compass)
+  require 'sassy-maps'
+  Compass::Frameworks.register(
+    "layout_tools",
+    :path => base_directory
+  )
+else
+  ENV["SASS_PATH"] = [ENV["SASS_PATH"], layout_tools_stylesheets_path].compact.join(File::PATH_SEPARATOR)
+end
+
+module LayoutTools
+  VERSION = "1.0.0"
+  DATE = "2015-12-01"
+end
